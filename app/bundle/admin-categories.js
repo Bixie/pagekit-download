f3881531e188412c15436fb66bcbd258c87d1d89
malles <matthijs@bixie.nl>
2015-12-15 18:02:28 +0100

    fix v-validate

--- app/bundle/admin-categories.js
+++ app/bundle/admin-categories.js
@@ -67,7 +67,7 @@
 
 	        UIkit.nestable(this.$els.nestable, {
 	            maxDepth: 20,
-	            group: 'site.nodes'
+	            group: 'download.categories'
 	        }).on('change.uk.nestable', function (e, nestable, el, type) {
 
 	            if (type && type !== 'removed') {
